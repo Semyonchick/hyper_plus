3c2d3559badaed5ecde59323a711e8fae123d10f
semyonchick <semyonchick@gmail.com>
2017-11-21 18:39:52 +0500

    bitrix
    Former-commit-id: 09650dbc01113b0396840f9695665259a3870679

--- init.js
+++ init.js
@@ -2,6 +2,6 @@ requirejs.config({
     baseUrl: 'https://smartsam.ru/hyper_plus/lib'
 });
 
-requirejs(['datepicker', 'backup', 'crmfields', 'noanswer']);
+requirejs(['datepicker', 'backup', 'crmfields', 'noanswer', 'bitrix']);
 
 console.log('hyper_plus init');
