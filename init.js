1a0730172587d32629fbdb9fc7509cf3aca4114c
semyonchick <semyonchick@gmail.com>
2017-11-24 12:17:52 +0500

    bitrix

--- init.js
+++ init.js
@@ -1,5 +1,6 @@
 requirejs.config({
-    baseUrl: 'https://smartsam.ru/hyper_plus/lib'
+    baseUrl: 'https://smartsam.ru/hyper_plus/lib',
+    urlArgs: "v=201724111211"
 });
 
 requirejs(['datepicker', 'backup', 'crmfields', 'noanswer', 'bitrix']);
